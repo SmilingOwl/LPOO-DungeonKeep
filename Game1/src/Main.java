b14bbca3a08778a057e124db86e23e35e3ecfcb0
Juliana Marques <julianamcm05@gmail.com>
2018-02-17 11:07:36 +0000

    added main function, print board function,print legend and directions, initial hero and guard coordinates

--- Game1/src/Main.java
+++ Game1/src/Main.java
@@ -3,43 +3,47 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		//imprimir legenda
-		
+
+		Scanner option = new Scanner(System.in);
+		int option1 = 0;
+		boolean state = true;
+
+		// coordenadas do heroi
+
+		int x_heroi = 1;
+		int y_heroi = 1;
+
+		// coordenadas do guarda
+
+		int xGuarda = 1;
+		int yGuarda = 8;
+		int passoGuarda = 0;
+
+		char[][] board = new char[][] { { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
+				{ 'X', 'H', ' ', ' ', 'I', ' ', 'X', ' ', 'G', 'X' },
+				{ 'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X' },
+				{ 'X', ' ', 'I', ' ', 'I', ' ', 'X', ' ', ' ', 'X' },
+				{ 'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X' },
+				{ 'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+				{ 'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+				{ 'X', 'X', 'X', ' ', 'X', 'X', 'X', 'X', ' ', 'X' },
+				{ 'X', ' ', 'I', ' ', 'I', ' ', 'X', 'K', ' ', 'X' },
+				{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' } };
+
+		// imprimir legenda
+
 		System.out.println("Legend:");
 		System.out.println("X -> Wall");
 		System.out.println("I -> Door");
 		System.out.println("H -> Hero");
 		System.out.println("G -> Guard");
 		System.out.println("K -> Lever");
-		System.out.println("emplty cell - free space");	
+		System.out.println("emplty cell - free space");
 		System.out.println("\n\n");
-		
-		char[][] board = new char[][] { {'X','X','X','X','X','X','X','X','X','X'},
-			{'X','H',' ',' ','I',' ','X',' ','G','X'},
-			{'X','X','X',' ','X','X','X',' ',' ','X'},
-			{'X',' ','I',' ','I',' ','X',' ',' ','X'},
-			{'X','X','X',' ','X','X','X',' ',' ','X'},
-			{'I',' ',' ',' ',' ',' ',' ',' ',' ','X'},
-			{'X','X','X',' ','X','X','X',' ',' ','X'},
-			{'X','X','X',' ','X','X','X','X',' ','X'},
-			{'X',' ','I',' ','I',' ','X','K',' ','X'},
-			{'X','X','X','X','X','X','X','X','X','X'} };
-			
-		
-		
-		//Imprimir o tabuleiro
-		
-for( int i =0; i<board.length;i++) 
-{
-	for(int j=0; j<board[i].length;j++) 
-	{
-		System.out.print(board[i][j]+" ");
-	}
-	System.out.println();
-	}
 
-		//perguntar ao utilizador a direção
+		printBoard(board);
+
+		// perguntar ao utilizador a direção
 		System.out.println();
 		System.out.println("Please choose a direction for your hero!");
 		System.out.println("options: ");
@@ -48,39 +52,14 @@ for( int i =0; i<board.length;i++)
 		System.out.println("Left -> press 3");
 		System.out.println("Right -> press 4");
 		System.out.println();
-		
-	Scanner option =new Scanner(System.in);
-	int option1 =option.nextInt();
-
-	//coordenadas do heroi
-	
-	int x_heroi=1;
-	int y_heroi=1;
-	
-	switch(option1)
-	{
-	case 1:
-		if(board[x_heroi-1][y_heroi]=='X'|| board[x_heroi-1][y_heroi]=='I')
-			break;
-		else 
-		{
-			board[x_heroi][y_heroi]=' ';
-			board[x_heroi-1][y_heroi]='H';
-			x_heroi=x_heroi-1;
-			
+	}
+	// método imprimir tabuleiro
+		public static void printBoard(char[][] board) {
+			for (int i = 0; i < board.length; i++) {
+				for (int j = 0; j < board[i].length; j++) {
+					System.out.print(board[i][j] + " ");
+				}
+				System.out.println();
+			}
 		}
-		break;
-	case 2:
-		break;
-	case 3:
-		break;
-	case 4:
-		break;
-	default : 
-		System.out.println("Invalid option!");
-		break;
-	}
-	
-	
-	}
 }
